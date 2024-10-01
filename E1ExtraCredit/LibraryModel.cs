0a74b7ec2b36c065a7ee3520d43d66b4cc8bd14c
varelaj1211 <varelaj@ksu.edu>
2024-10-01 12:09:09 -0500

    Start

---
+++ E1ExtraCredit/LibraryModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E1ExtraCredit
+{
+    public class LibraryModel
+    {
+    }
+
+    public class Book
+    {
+        private int _page { get; set; }
+        private string _title { get; set; }
+
+        private string _author { get; set; }
+
+        private string _serialNumber;
+
+        public Book() { }   
+    }
+}
